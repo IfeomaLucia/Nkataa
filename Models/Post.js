6de417c37da7c78a51499c16d9c5c86e4c3beddb
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-06-25 12:23:49 +0100

    Gotta comment my code

--- Models/Post.js
+++ Models/Post.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var PostSchema = mongoose.Schema({
     time: Date,
     postBody: String,
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    comments: [{type: mongoose.Schema.Types.ObjectId, ref:'Comment'}]
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},//refrences the user file
+    comments: [{type: mongoose.Schema.Types.ObjectId, ref:'Comment'}]//refrences the comment file
 })
 
 module.exports = mongoose.model('Post', PostSchema);
