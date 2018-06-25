72adb91f030d9542228cbc2f5c279c72f49416e1
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-06-25 13:51:42 +0100

    added the route for deleting

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ var userController = require('../Controllers/UserController');
 /* GET users listing. */
 router.post('/create', userController.addUser);
 
+router.get('/', userController.getUsers);
+
+router.get('/delete/:id', userController.deleteUser);
+
 module.exports = router;
