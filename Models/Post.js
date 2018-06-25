762b134aac6690d36a1b695a40628886d15e07b9
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-06-25 12:29:04 +0100

    syntax change

--- Models/Post.js
+++ Models/Post.js
@@ -4,7 +4,7 @@ var PostSchema = mongoose.Schema({
     time: Date,
     postBody: String,
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},//refrences the user file
-    comments: [{type: mongoose.Schema.Types.ObjectId, ref:'Comment'}]//refrences the comment file
+    comments: [{type: mongoose.Schema.Types.ObjectId, ref:'Comment'}]//refrences the multiple comments made by a single user
 })
 
 module.exports = mongoose.model('Post', PostSchema);
