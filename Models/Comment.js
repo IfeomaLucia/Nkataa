3d61efe8ff747ab79432d85daccce4b39173801f
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-06-25 12:28:44 +0100

    Additional objects

--- Models/Comment.js
+++ Models/Comment.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 var CommentSchema = mongoose.Schema({
     name: String,
     time: Date,
-    commentBody: String
+    commentBody: String,
+    user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},//indicates the user making the comment
+    post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
 })
 
 module.exports = mongoose.model('Comment', CommentSchema);
