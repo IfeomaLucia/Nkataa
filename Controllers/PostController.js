0813ccb4f4f6da2613fa0878834b5353034fd51e
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-06-26 15:28:21 +0100

    added an update posts function

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -27,13 +27,13 @@ exports.deletePost = function(req, res){
     });
 }
 
-// exports.updatePost = function(req, res){
-//     var id = req.params.id;
-//     var update = {
-//         postBody: req.body.postBody
-//     };
-//     model.findByIdAndUpdate(id, update, function(err){
-//         if(err) res.json({err: err, message: 'Update error'});
-//         res.json({message: update});
-//     });
-// }
+exports.updatePost = function(req, res){
+    var id = req.params.id;
+    var update = {
+        postBody: req.body.postBody
+    };
+    model.findByIdAndUpdate(id, update, function(err){
+        if(err) res.json({err: err, message: 'Update error'});
+        res.json({message: update});
+    });
+}
