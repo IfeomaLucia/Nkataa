193cfa40bd13ead3670faaaca38817f8a740839a
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-06-26 16:18:05 +0100

    necessary routes for the comments

---
+++ routes/comments.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var commentController = require('../Controllers/CommentController');
+
+/* GET users listing. */
+router.post('/create', commentController.addComment);
+
+router.get('/', commentController.getComments);
+
+router.get('/delete/:id', commentController.deleteComment);
+
+router.post('/update/:id', commentController.updateComment);
+
+module.exports = router;
