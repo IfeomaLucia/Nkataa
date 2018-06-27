131ca4b60aeb478c56389bfd18cd151b637bbb2f
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-06-27 08:10:13 +0100

    added the file

--- Controllers/CommentController.js
+++ Controllers/CommentController.js
@@ -5,7 +5,8 @@ exports.addComment = function(req, res){
         name: req.body.name,
         time: Date.now(),
         commentBody: req.body.commentBody,
-        user: req.body.user
+        user: req.body.user,
+        post: req.body.post
     };
     model.create(data, function(err){
         if(err) res.json({err: err, message:'Comment could not be created'});
@@ -20,6 +21,14 @@ exports.getComments = function(req, res){
     });
 }
 
+// exports.getCommentsByPostId = function(req, res){
+//     var id = req.params.id;
+//     model.findById(id, function(err, data){
+//         if(err) res.json({err: err, message:'something went wrong'});
+//         res.json(data);
+//     });
+// }
+
 exports.deleteComment = function(req, res){
     var options = {_id: req.params.id};
     model.remove(options, function(err){
@@ -28,13 +37,3 @@ exports.deleteComment = function(req, res){
     });
 }
 
-exports.updateComment = function(req, res){
-    var id = req.params.id;
-    var update = {
-        commentBody: req.body.commentBody
-    };
-    model.findByIdAndUpdate(id, update, function(err){
-        if(err) res.json({err: err, message: 'Update error'});
-        res.json({message: update});
-    });
-}
