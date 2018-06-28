23f6125da9149ef5e3d6eea85accc630e7bdfc57
IfeomaLucia <nwosuifeomalucia@gmail.com>
2018-06-28 16:30:27 +0100

    created a search route

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ var commentController = require('../Controllers/CommentController');
 /* GET users listing. */
 router.get('/', commentController.getComment);
 router.post('/create', commentController.addComment);
-// router.get('/:id', commentController.getCommentsByPostId);
+router.get('/search/', commentController.getCommentsById);
 router.get('/delete/', commentController.deleteComment);
 router.post('/update/', commentController.updateComment);
 
